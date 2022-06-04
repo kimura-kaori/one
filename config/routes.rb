c7cd2d872b6d79a61f81a61a33c006b1a0dc82fe
kaori kimura <tkhy_1125@icloud.com>
2022-06-04 21:36:58 +0900

    仮トップページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  root 'top#index'
   devise_for :users
-  root 'students#index'
   resources :students
-  
+
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
