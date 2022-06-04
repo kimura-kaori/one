207388d838d0bce9f167335b6fa1a6b9b362a2aa
kaori kimura <tkhy_1125@icloud.com>
2022-06-04 16:49:35 +0900

    studentテーブルに郵便番号カラムを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-10.times do |n|
-  email = Faker::Internet.email
-  password = "password"
-  User.create!(email: email,
-               password: password,
-               password_confirmation: password,
-               admin: false,
-               )
-end
+# 10.times do |n|
+#   email = Faker::Internet.email
+#   password = "password"
+#   User.create!(email: email,
+#                password: password,
+#                password_confirmation: password,
+#                admin: false,
+#                )
+# end
+# 10.times do |i|
+#   Student.create!(
+#     : "test#{i + 1}",
+#     content: "テスト",
+#     deadline: "2022-05-05",
+#     priority: rand(0..2),
+#     status: rand(0..2),
+#     user_id: rand(1..10),
+#     label_ids: rand(1..10)
+# )
+# end
