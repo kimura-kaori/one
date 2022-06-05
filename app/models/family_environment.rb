15e55210568fe3e996d007284ec176c40482f94e
kaori kimura <tkhy_1125@icloud.com>
2022-06-05 16:51:53 +0900

    生徒情報と家庭環境情報のアソシエーションを実装

--- app/models/family_environment.rb
+++ app/models/family_environment.rb
@@ -1,3 +1,4 @@
 class FamilyEnvironment < ApplicationRecord
   has_many :student_family_environment, dependent: :destroy
+  has_many :student_family_environment_students, through: :student_family_environments, source: :student
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,5 @@
 class Student < ApplicationRecord
   belongs_to :user
   has_many :student_family_environment, dependent: :destroy
+  # has_many :student_family_environment_family_environments, through: :student_family_environments, source: :family_environment
 end
