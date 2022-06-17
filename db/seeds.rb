35c0bd1615f81edc841037a4efec64ec63b93404
kaori kimura <tkhy_1125@icloud.com>
2022-06-17 23:09:38 +0900

    seedデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,32 +18,138 @@
 end
 User.create(email: "admin@example.com", password: "111111", admin: true, confirmed_at: Time.now)
 
-10.times do |n|
-  student_furigana = Gimei.name.hiragana
-  student_name = Gimei.name.kanji
-  sex = Faker::Number.between(from: 1, to: 2)
-  birthday = Faker::Date.between(from: '1959-1-1', to: '2001-12-31')
-  telephone = '0'+ Faker::Lorem.characters(number: 10, min_numeric: 10)
-  cellphone = '0'+ Faker::Lorem.characters(number: 10, min_numeric: 10)
-  post_code = Faker::Lorem.characters(number: 3, min_numeric: 3) + '-' + Faker::Lorem.characters(number: 4, min_numeric: 4)
-  address = Gimei.address.city.kanji + Gimei.address.town.kanji + Faker::Lorem.characters(number: 1, min_numeric: 1) + '-' + Faker::Lorem.characters(number: 2, min_numeric: 2)
-  parents_furigana = Gimei.hiragana
-  parents_name = Gimei.name.kanji
-  relationship = '祖母'
-  Student.create!(student_furigana: student_furigana,
-                  student_name: student_name,
-                  sex: sex,
-                  birthday: birthday,
-                  telephone: telephone,
-                  cellphone: cellphone,
-                  post_code: post_code,
-                  address: address,
-                  parents_furigana: parents_furigana,
-                  parents_name: parents_name,
-                  relationship: relationship,
-                  user_id: User.find(n+1).id,
-                  )
-end
+Student.create!(student_furigana: 'ひの あやみ',
+                student_name: '日野彩美',
+                sex: '女',
+                birthday: '2011年4月8日',
+                telephone: '075-123-4567',
+                cellphone: '090-9876-5432',
+                post_code: '600-0001',
+                address: '京都府京都市中京区御所南123',
+                parents_furigana: 'ひのかよこ',
+                parents_name: '日野香代子',
+                relationship: '母',
+                user_id: 1)
+Student.create!(student_furigana: 'ひのとおる',
+                student_name: '日野透',
+                sex: '男',
+                birthday: '2008年1月8日',
+                telephone: '075-123-4567',
+                cellphone: '090-9876-5432',
+                post_code: '600-0001',
+                address: '京都府京都市中京区御所南123',
+                parents_furigana: 'ひのかよこ',
+                parents_name: '日野香代子',
+                relationship: '母',
+                user_id: 1)
+Student.create!(student_furigana: 'おくのひさの',
+                student_name: '奥野久乃',
+                sex: '女',
+                birthday: '2011年6月8日',
+                telephone: '075-674-4567',
+                cellphone: '090-1234-5432',
+                post_code: '600-0000',
+                address: '京都府京都市中京区落合5-68',
+                parents_furigana: 'おくのたくや',
+                parents_name: '奥野拓也',
+                relationship: '父',
+                user_id: 2)
+Student.create!(student_furigana: 'しまむらそうすけ',
+                student_name: '島村宗介',
+                sex: '男',
+                birthday: '2011年6月11日',
+                telephone: '075-674-1234',
+                cellphone: '090-5432-6789',
+                post_code: '600-0000',
+                address: '京都府京都市中京区吉見町十和田山根0-79',
+                parents_furigana: 'しまむらただし',
+                parents_name: '島村正',
+                relationship: '父',
+                user_id: 3)
+Student.create!(student_furigana: 'ふくしまゆうき',
+                student_name: '福島勇気',
+                sex: '男',
+                birthday: '2011年12月11日',
+                telephone: '075-123-1234',
+                cellphone: '090-5432-6789',
+                post_code: '600-0000',
+                address: '京都府京都市中京区日高町太田9-12',
+                parents_furigana: 'ふくしまのぶゆき',
+                parents_name: '福島信幸',
+                relationship: '父',
+                user_id: 4)
+Student.create!(student_furigana: 'はやかわしんじ',
+                student_name: '早川慎二',
+                sex: '男',
+                birthday: '2011年8月11日',
+                telephone: '075-985-1234',
+                cellphone: '090-358-6789',
+                post_code: '600-0000',
+                address: '京都府京都市中京区日高町太田5-6',
+                parents_furigana: 'はやかわたろう',
+                parents_name: '早川太郎',
+                relationship: '父',
+                user_id: 5)
+Student.create!(student_furigana: 'はやかわさき',
+                student_name: '早川咲',
+                sex: '女',
+                birthday: '2004年12月11日',
+                telephone: '075-985-1234',
+                cellphone: '090-358-6789',
+                post_code: '600-0000',
+                address: '京都府京都市中京区日高町太田5-6',
+                parents_furigana: 'はやかわたろう',
+                parents_name: '早川太郎',
+                relationship: '父',
+                user_id: 5)
+Student.create!(student_furigana: 'はやかわそう',
+                student_name: '早川奏',
+                sex: '男',
+                birthday: '2005年10月1日',
+                telephone: '075-985-1234',
+                cellphone: '090-358-6789',
+                post_code: '600-0000',
+                address: '京都府京都市中京区日高町太田5-6',
+                parents_furigana: 'はやかわたろう',
+                parents_name: '早川太郎',
+                relationship: '父',
+                user_id: 5)
+Student.create!(student_furigana: 'おがたもとよ',
+                student_name: '尾形素美',
+                sex: '女',
+                birthday: '2011年3月19日',
+                telephone: '075-358-1234',
+                cellphone: '090-853-6789',
+                post_code: '600-0000',
+                address: '京都府京都市中京区上勝町1445',
+                parents_furigana: 'おがたあきひろ',
+                parents_name: '尾形明宏',
+                relationship: '父',
+                user_id: 6)
+Student.create!(student_furigana: 'たかぞのれい',
+                student_name: '高園怜',
+                sex: '女',
+                birthday: '2011年3月19日',
+                telephone: '075-358-1234',
+                cellphone: '090-853-6789',
+                post_code: '600-0000',
+                address: '京都府京都市中京区代官山町1445',
+                parents_furigana: 'たかぞのよしき',
+                parents_name: '高園芳樹',
+                relationship: '父',
+                user_id: 7)
+Student.create!(student_furigana: 'たかぞのなぎさ',
+                student_name: '高園渚',
+                sex: '男',
+                birthday: '2011年7月19日',
+                telephone: '075-358-1234',
+                cellphone: '090-853-6789',
+                post_code: '600-0000',
+                address: '京都府京都市中京区代官山町1445',
+                parents_furigana: 'たかぞのよしき',
+                parents_name: '高園芳樹',
+                relationship: '父',
+                user_id: 7)
 
 10.times do |n|
   name = Gimei.kanji
@@ -68,7 +174,7 @@ end
                            student_id: Student.find(n+1).id,
                            )
 end
-
+#
 10.times do |n|
   Contact.create!(title: "test#{n + 1}",
                   user_id: User.find(n+1).id,
