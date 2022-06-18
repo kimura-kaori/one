5787ea91f7b6b06cd2ad9ecc3d61245f8277f398
kaori kimura <tkhy_1125@icloud.com>
2022-06-18 09:24:37 +0900

    commentcontrollerのcomment_paramsのコード修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,6 +18,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content).merge(current_user.id)
+    params.require(:comment).permit(:content).merge(user_id: current_user.id)
   end
 end
